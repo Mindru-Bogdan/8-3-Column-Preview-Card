5644f7bf9ef9e4eba4a33c61d4bdba3066982165
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-05-12 20:36:47 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,14 @@
+import SedansLogo from '../../src/icon-sedans.svg';
+import SuvsLogo from '../../src/icon-suvs.svg';
+import LuxuryLogo from '../../src/icon-luxury.svg';
+
 import classes from './Card.module.css';
 
 const Card = () => {
   return (
     <div>
-      <div>
+      <div className={classes.firstColumn}>
+        <img src={SedansLogo} />
         <h1 className={classes.title}>SEDANS</h1>
         <p className={classes.description}>
           Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next
@@ -11,7 +16,8 @@ const Card = () => {
         </p>
         <p className={classes.learnMore}>Learn More</p>
       </div>
-      <div>
+      <div className={classes.secondColumn}>
+        <img src={SuvsLogo} />
         <h1 className={classes.title}>SUVS</h1>
         <p className={classes.description}>
           Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road
@@ -19,7 +25,8 @@ const Card = () => {
         </p>
         <p className={classes.learnMore}>Learn More</p>
       </div>
-      <div>
+      <div className={classes.thirdColumn}>
+        <img src={LuxuryLogo} />
         <h1 className={classes.title}>LUXURY</h1>
         <p className={classes.description}>
           Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive
@@ -27,6 +34,7 @@ const Card = () => {
         </p>
         <p className={classes.learnMore}>Learn More</p>
       </div>
+      <div className={classes.whiteSpace}></div>
     </div>
   );
 };
